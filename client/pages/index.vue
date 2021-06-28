3841f9c0d1a664be1c4a897d1c173d3469fe21b2
Marlak's Computer <mik.myk2015@gmail.com>
2021-06-28 11:04:11 +0400

    fixing my nuxt mistake v2

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -98,9 +98,6 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
   middleware: ['auth'],
   async fetch({store}) {
